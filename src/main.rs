82cbe87be8dfae6347c8cfa50a16b109eff3f3ae
louis <louis.turpault@gmail.com>
2025-03-09 22:29:25 -0400

    use --release for good perf

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use image::Luma;
 use qrcode::QrCode;
-use std::fmt::Error;
 use std::fs::File;
 use std::io::Read;
-use std::io::Seek;
-use std::io::SeekFrom;
 
-fn create_qr_code_from_data(data: [u8; 1024], qr_number: u8) {
+fn create_qr_code_from_data(data: [u8; 2048], qr_number: u16) {
     let data_byte_string_litteral: &[u8] = &data;
-    let code = QrCode::new(data_byte_string_litteral).unwrap();
+    let code: QrCode = QrCode::new(data_byte_string_litteral).unwrap();
+    // .render()
+    // .quiet_zone(false)
+    // .build();
+
     let mut file_string: String = String::from("/tmp/qrcode{}.png");
     let qr_number_string = qr_number.to_string();
     // Render the bits into an image.
@@ -19,22 +20,16 @@ fn create_qr_code_from_data(data: [u8; 1024], qr_number: u8) {
 
 fn get_data_from_file(string_of_file: String) {
     let mut data_counter: u64 = 0;
-    let mut qrcode_counter: u8 = 0;
+    let mut qrcode_counter: u16 = 0;
     let mut file = match File::open(string_of_file) {
         Ok(file) => file,
         Err(err) => panic!("Error opening file: {}", err),
     };
-
-    while data_counter < file.metadata().unwrap().len() {
-        let offset: u64 = data_counter;
-        match file.seek(SeekFrom::Start(offset)) {
-            Ok(_) => println!("Seeked to offset {}", offset),
-            Err(err) => panic!("Error seeking: {}", err),
-        };
-
-        let mut buffer = [0; 1024];
+    let mut buffer = [0; 2048];
+    let file_length = file.metadata().unwrap().len();
+    while data_counter < file_length {
         match file.read(&mut buffer) {
-            Ok(n) => println!("Read {} bytes", n),
+            Ok(n) => println!("Read {} bytes out of {}", n, file_length),
             Err(err) => panic!("Error reading file: {}", err),
         };
         create_qr_code_from_data(buffer, qrcode_counter);
